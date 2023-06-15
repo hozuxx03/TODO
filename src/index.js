f73341fdfd319b2c6030b231c673efe2596fdb7d
hozu <106940992+hozuxx03@users.noreply.github.com>
2023-06-15 13:24:15 +0000

    Update 3 files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const App = () => {
+  return <h1>あ</h1>;
+};
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
